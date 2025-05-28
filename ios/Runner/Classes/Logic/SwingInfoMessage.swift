d05e2107230ce8eefe9c29d25268f47bc107f522
gakutakimoto <59184937+gakutakimoto@users.noreply.github.com>
2025-05-28 15:21:49 +0900

    refactor: update swing info json handling for Xcode 15

---
+++ ios/Runner/Classes/Logic/SwingInfoMessage.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct SwingInfoMessage: Codable {
+    let isExist: Bool
+    let impactId: UInt
+    let index: UInt
+    let hs: Double
+    let angle1: Double
+    let angle2: Double
+    let angle3: Double
+}
+
